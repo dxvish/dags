448763d6ab18e04d910c35d3d4b21e51d76adc94
dxvish <dxvish@gmail.com>
2023-12-07 19:27:42 +0530

    Pushed DAG untitled1-1207192740

---
+++ untitled1-1207192740.py
@@ -0,0 +1,31 @@
+from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+args = {
+    "project_id": "untitled1-1207192740",
+}
+
+dag = DAG(
+    "untitled1-1207192740",
+    default_args=args,
+    schedule_interval="@once",
+    start_date=days_ago(1),
+    description="""
+Created with Elyra 3.15.0 pipeline editor using `untitled1.pipeline`.
+    """,
+    is_paused_upon_creation=False,
+)
+
+
+# Operator source: {"catalog_type": "airflow-package-catalog", "component_ref": {"airflow_package": "apache_airflow-1.10.15-py2.py3-none-any.whl", "file": "airflow/operators/bash_operator.py"}}
+
+op_9f141664_efd9_40f5_b8fb_1764abc2dbca = BashOperator(
+    task_id="BashOperator",
+    bash_command="ls",
+    xcom_push=True,
+    env={},
+    output_encoding="utf-8",
+    executor_config={},
+    dag=dag,
+)
